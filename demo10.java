67116675a15e13de319201bd278643b595cf811f
Adi7yasingh <aditya9826864695@gmail.com>
2024-09-10 17:27:31 +0530

    new

--- demo10.java
+++ demo10.java
@@ -4,14 +4,15 @@ class demo10
     public static void main(String[] args)
     {
        // int num1[] = {5,6,7};
-       int num1[] = new int[4];
+       int num1[] = new int[5];
        num1[0] = 4;
        num1[1] = 5;
        num1[2] = 6;
        num1[3] = 7;
+       num1[4] = 8;
        //modified this   
 
-       for(int i=0;i<=3;i++)
+       for(int i=0;i<=4;i++)
        {
             System.out.println(num1[i]);
        }
---
+++ demo11.java
@@ -0,0 +1,8 @@
+class demo11
+{
+    public static void main(String[] args)
+    {
+        //multidimensional array
+        
+    }
+}
